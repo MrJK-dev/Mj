7dce88ae4c63fed6f3c01c738765213bbb3afe9f
MidKing01 <139042897+Joshuamambo1@users.noreply.github.com>
2025-08-06 22:28:37 +0200

    Add files via upload

---
+++ zandile.js
@@ -0,0 +1,20 @@
+var commands = [];
+
+function zandile(info, func) {
+    var data = info;
+    data.function = func;
+    if (!data.dontAddCommandList) data.dontAddCommandList = false;
+    if (!info.desc) info.desc = '';
+    if (!data.fromMe) data.fromMe = false;
+    if (!info.category) data.category = 'misc';
+    if(!info.filename) data.filename = "Not Provided";
+    commands.push(data);
+    return data;
+}
+module.exports = {
+    zandile,
+    AddCommand:zandile,
+    Function:zandile,
+    Module:zandile,
+    commands,
+};
